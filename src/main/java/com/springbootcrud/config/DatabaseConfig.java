9974d4caf2353acc23c8ff20099aab19dce467e6
Ankesh Pandey <ankeshpandey987@gmail.com>
2019-09-24 14:19:02 +0530

    Update DatabaseConfig.java

--- src/main/java/com/springbootcrud/config/DatabaseConfig.java
+++ src/main/java/com/springbootcrud/config/DatabaseConfig.java
@@ -1,23 +1,26 @@
-package com.springbootcrud.config;
+//   Since we are added the database configuration in our application.properties, we don't need this.
 
-import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-
-@Configuration
-@Component
-public class DatabaseConfig {
-
-	@ConfigurationProperties(prefix = "spring.datasource")
-	@Bean
-	@Primary
-	public DataSource getDataSource() {
-		return DataSourceBuilder.create().url("jdbc:mysql://localhost:3306/dbspringboot?useSSL=false").username("root")
-				.password("2015").build();
-	}
-}
+//package com.springbootcrud.config;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Component;
+//
+//@Configuration
+//@Component
+//public class DatabaseConfig {
+//
+//	@ConfigurationProperties(prefix = "spring.datasource")
+//	@Bean
+//	@Primary
+//	public DataSource getDataSource() {
+//		return DataSourceBuilder.create().url("jdbc:mysql://localhost:3306/dbspringboot?useSSL=false").username("root")
+//				.password("2015").build();
+//	}
+//}
